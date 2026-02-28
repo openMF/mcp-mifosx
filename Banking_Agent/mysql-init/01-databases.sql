0e1f581f80b94e81ded486d959a767623e8c4368
gyanLM10 <gyankbhuyan@gmail.com>
2026-02-28 11:01:22 +0530

    chore: untrack config and cache files in Banking_Agent gitignore

--- Banking_Agent/mysql-init/01-databases.sql
+++
@@ -1,7 +0,0 @@
--- Create required Fineract databases
-CREATE DATABASE IF NOT EXISTS `fineract_tenants`;
-
-CREATE DATABASE IF NOT EXISTS `fineract_default`;
-
--- Grant all privileges to root from any host
-GRANT ALL ON *.* TO 'root' @'%';
